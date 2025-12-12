8897d9242623a67c03880c52651fc147ab9a2a75
XinshenXiangChen <xinshenxiangchen@gmail.com>
2025-12-13 00:22:12 +0100

    matplotlib business

---
+++ NN.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+from torch.nn import Sequential
+
+class NN(nn.Module):
+    def __init__(self):
+        super(NN, self).__init__()
+
+    def forward(self, x):
+        pass
--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import kagglehub
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
 # Download latest version
 path = kagglehub.dataset_download("meharshanali/amazon-stocks-2025")
@@ -10,3 +11,24 @@ csv_path = os.path.join(path, "AMZN_stock_data.csv")
 
 
 dataset = pd.read_csv(csv_path)
+
+if __name__ == "__main__":
+    print(dataset["Close"])
+    close = dataset["Close"]
+    high = dataset["High"]
+
+    fig, axs = plt.subplots(2, 2, figsize=(10,8))  # 2x2 grid
+    axs[0, 0].plot(close)
+    axs[0, 0].set_title('Close')
+    axs[0, 1].scatter(close, close - high, color='red')
+    axs[0, 1].set_title('Close - High')
+
+    plt.show()
+
+
+    print(dataset.index)
+    print(dataset.columns)
+    print(dataset.loc[0])
+
+
+
